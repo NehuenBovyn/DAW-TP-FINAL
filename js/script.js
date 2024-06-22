d4b7a760bfd88943fd66a27e23745b86973395e8
Nehuen <bovynnehuen@gmail.com>
2024-06-22 11:09:54 -0300

    boton de contacto y redireccion

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
   redirectToGithub();
   redirectToOtherPag('btn-start', 'pages/game.html');
+  redirectToOtherPag('contacto-btn', '/pages/contacto.html');
 });
 
 function redirectToGithub() {
   document.getElementById("github-btn").onclick = function() {
-      window.open("https://github.com/NehuenBovyn/DAW-TP-FINAL", "_blank");
+    window.open("https://github.com/NehuenBovyn/DAW-TP-FINAL", "_blank");
   };
 }
 
 function redirectToOtherPag(idHtmlElement, url) {
-  document.getElementById(idHtmlElement).addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = url;
-  });
+  var element = document.getElementById(idHtmlElement);
+  if (element) {
+    element.addEventListener('click', function(event) {
+      event.preventDefault();
+      window.location.href = url;
+    });
+  }
 }
