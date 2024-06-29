5f50382cd73cadb98cc62fa37701169d13bf4034
Nehuen <bovynnehuen@gmail.com>
2024-06-29 20:27:09 -0300

    puntuacion y timer

--- js/boogle.js
+++ js/boogle.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var grid = document.getElementById('grid-container');
   var wordInput = document.getElementById('word');
@@ -13,127 +13,201 @@ document.addEventListener('DOMContentLoaded', function() {
   var submitMessage = document.getElementById('submit-message');
 
   function generateGrid() {
-      grid.innerHTML = '';
-      selectedButtons = [];
-      for (var i = 0; i < 16; i++) {
-          var letter = letters.charAt(Math.floor(Math.random() * letters.length));
-          var button = document.createElement('button');
-          button.className = 'grid-item';
-          button.textContent = letter;
-          button.addEventListener('click', function() {
-              handleButtonClick(this);
-          });
-          grid.appendChild(button);
-      }
+    grid.innerHTML = '';
+    selectedButtons = [];
+    for (var i = 0; i < 16; i++) {
+      var letter = letters.charAt(Math.floor(Math.random() * letters.length));
+      var button = document.createElement('button');
+      button.className = 'grid-item';
+      button.textContent = letter;
+      button.addEventListener('click', function () {
+        handleButtonClick(this);
+      });
+      grid.appendChild(button);
+    }
   }
 
   function handleButtonClick(button) {
-      if (selectedButtons.length === 0 || isValidMove(button)) {
-          var letter = button.textContent;
-          addToWord(letter);
-          selectedButtons.push(button);
-          button.disabled = true;
-      } else {
-          showSubmitMessage('Invalid move! Select an adjacent letter.');
-      }
+    if (selectedButtons.length === 0 || isValidMove(button)) {
+      var letter = button.textContent;
+      addToWord(letter);
+      selectedButtons.push(button);
+      button.disabled = true;
+    } else {
+      showSubmitMessage('Movimiento inválido.');
+    }
   }
 
   function isValidMove(button) {
-      if (selectedButtons.length === 0) {
-          return true;
-      }
-      var lastButton = selectedButtons[selectedButtons.length - 1];
-      var lastButtonIndex = Array.from(grid.children).indexOf(lastButton);
-      var currentButtonIndex = Array.from(grid.children).indexOf(button);
-      var lastIndexRow = Math.floor(lastButtonIndex / 4);
-      var lastIndexCol = lastButtonIndex % 4;
-      var currentRow = Math.floor(currentButtonIndex / 4);
-      var currentCol = currentButtonIndex % 4;
+    if (selectedButtons.length === 0) {
+      return true;
+    }
+    var lastButton = selectedButtons[selectedButtons.length - 1];
+    var lastButtonIndex = Array.prototype.indexOf.call(
+      grid.children,
+      lastButton
+    );
+    var currentButtonIndex = Array.prototype.indexOf.call(
+      grid.children,
+      button
+    );
+    var lastIndexRow = Math.floor(lastButtonIndex / 4);
+    var lastIndexCol = lastButtonIndex % 4;
+    var currentRow = Math.floor(currentButtonIndex / 4);
+    var currentCol = currentButtonIndex % 4;
 
-      return Math.abs(lastIndexRow - currentRow) <= 1 && Math.abs(lastIndexCol - currentCol) <= 1;
+    return (
+      Math.abs(lastIndexRow - currentRow) <= 1 &&
+      Math.abs(lastIndexCol - currentCol) <= 1
+    );
   }
 
   function addToWord(letter) {
-      var currentWord = wordInput.value;
-      wordInput.value = currentWord + letter;
+    var currentWord = wordInput.value;
+    wordInput.value = currentWord + letter;
   }
 
   function submitWord() {
-      var word = wordInput.value.toLowerCase().trim();
-      if (word.length >= 3) {
-          var uniqueLetters = new Set(word);
-          if (uniqueLetters.size === word.length) {
-              var apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-              fetch(apiUrl)
-                  .then(function(response) {
-                      if (response.ok) {
-                          return response.json();
-                      } else {
-                          throw new Error('Word not found in dictionary.');
-                      }
-                  })
-                  .then(function(data) {
-                      console.log(data);
-                      points += 1;
-                      pointsDisplay.textContent = points;
-                      showSubmitMessage(`Word "${word}" found in dictionary! Points: ${points}`);
-                      wordsFound.push(word);
-                      updateWordsFoundList();
-                      clearSelectedButtons();
-                  })
-                  .catch(function(error) {
-                      console.error(error);
-                      points -= 1;
-                      pointsDisplay.textContent = points;
-                      showSubmitMessage(`Word "${word}" not found in dictionary. Points: ${points}`);
-                      clearSelectedButtons();
-                  });
-          } else {
-              showSubmitMessage('Word contains duplicate letters.');
-              clearSelectedButtons();
-          }
-      } else {
-          showSubmitMessage('Word must be at least 3 letters long.');
-          clearSelectedButtons();
+    var word = wordInput.value.toLowerCase().trim();
+    if (word.length >= 3) {
+      if (wordsFound.indexOf(word) !== -1) {
+        showSubmitMessage('La palabra ya fue descubierta.');
+        clearSelectedButtons();
+        return;
       }
+      var uniqueLetters = new Set(word);
+      if (uniqueLetters.size === word.length) {
+        var apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word;
+        fetch(apiUrl)
+          .then(function (response) {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error('La palabra no existe.');
+            }
+          })
+          .then(function () {
+            var pointGets = calculatePoints(word.length);
+            points += pointGets;
+            pointsDisplay.textContent = points;
+            showSubmitMessage(
+              'La palabra "' +
+                word +
+                '" existe! Sumas: ' +
+                pointGets +
+                ' puntos.'
+            );
+            wordsFound.push(word);
+            updateWordsFoundList();
+            clearSelectedButtons();
+          })
+          .catch(function (error) {
+            console.error(error);
+            points -= 1;
+            pointsDisplay.textContent = points;
+            showSubmitMessage(
+              'La palabra "' + word + '" no existe. Restas: 1 punto.'
+            );
+            clearSelectedButtons();
+          });
+      }
+    } else {
+      showSubmitMessage('La palabra debe contener al menos 3 letras.');
+      clearSelectedButtons();
+    }
+  }
+
+  function calculatePoints(wordLength) {
+    switch (wordLength) {
+      case 3:
+        return 1;
+      case 4:
+        return 1;
+      case 5:
+        return 2;
+      case 6:
+        return 3;
+      case 7:
+        return 5;
+      default:
+        return 11;
+    }
   }
 
   function showSubmitMessage(message) {
-      submitMessage.textContent = message;
-      submitMessage.classList.add('show-message');
-      setTimeout(function() {
-          submitMessage.classList.remove('show-message');
-      }, 2000);
+    var showMessageDiv = document.getElementById('show-message');
+    showMessageDiv.textContent = message;
+    setTimeout(function () {
+      showMessageDiv.textContent = '';
+    }, 2000);
   }
 
   function updateWordsFoundList() {
-      wordsFoundList.innerHTML = '';
-      wordsFound.forEach(function(word) {
-          var listItem = document.createElement('li');
-          listItem.textContent = word;
-          wordsFoundList.appendChild(listItem);
-      });
+    wordsFoundList.innerHTML = '';
+    wordsFound.forEach(function (word) {
+      var listItem = document.createElement('li');
+      listItem.textContent = word;
+      wordsFoundList.appendChild(listItem);
+    });
   }
 
   function clearSelectedButtons() {
-      selectedButtons.forEach(function(button) {
-          button.disabled = false;
-      });
-      selectedButtons = [];
-      wordInput.value = '';
+    selectedButtons.forEach(function (button) {
+      button.disabled = false;
+    });
+    selectedButtons = [];
+    wordInput.value = '';
   }
 
-  shuffleBtn.addEventListener('click', function() {
-      generateGrid();
-      shuffleMessage.classList.add('show-message');
-      setTimeout(function() {
-          shuffleMessage.classList.remove('show-message');
-      }, 2000);
-      wordInput.value = ''; 
+  shuffleBtn.addEventListener('click', function () {
+    generateGrid();
+    shuffleMessage.classList.add('show-message');
+    setTimeout(function () {
+      shuffleMessage.classList.remove('show-message');
+    }, 2000);
+    wordInput.value = '';
   });
 
-  submitWordBtn.addEventListener('click', function() {
-      submitWord();
+  submitWordBtn.addEventListener('click', function () {
+    submitWord();
+  });
+
+  document.addEventListener('timeUp', function () {
+    var playerName = localStorage.getItem('playerName');
+    var currentTime = new Date().toLocaleString();
+    var scoreData = {
+      playerName: playerName,
+      points: points,
+      timestamp: currentTime,
+    };
+    var scores = JSON.parse(localStorage.getItem('scores')) || [];
+    scores.push(scoreData);
+    localStorage.setItem('scores', JSON.stringify(scores));
+
+    // Mostrar el modal de tiempo agotado con la información recolectada
+    var timeUpModal = document.getElementById('time-up-modal');
+    var playerSpan = document.getElementById('time-up-player');
+    var pointsSpan = document.getElementById('time-up-points');
+    var timestampSpan = document.getElementById('time-up-timestamp');
+
+    playerSpan.textContent = playerName;
+    pointsSpan.textContent = points;
+    timestampSpan.textContent = currentTime;
+
+    timeUpModal.classList.add('active');
+  });
+
+  // Función para reiniciar el juego al hacer clic en el botón 'Empezar de Nuevo'
+  var restartBtn = document.getElementById('restart-btn');
+  restartBtn.addEventListener('click', function () {
+    // Lógica para reiniciar el juego según sea necesario
+    // Por ejemplo, resetear puntos, palabras encontradas, etc.
+    points = 0;
+    pointsDisplay.textContent = points;
+    wordsFound = [];
+    updateWordsFoundList();
+    generateGrid();
+    timeUpModal.classList.remove('active');
   });
 
   generateGrid();
--- js/game.js
+++ js/game.js
@@ -1,41 +1,82 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var overlay = document.querySelector('.popup-overlay');
+  var timerDisplay = document.getElementById('timer');
+  var countdownInterval;
+
   function showPopUp() {
-      overlay.style.display = 'block';
+    overlay.style.display = 'block';
   }
 
   function closePopUp() {
     overlay.style.display = 'none';
   }
+
   showPopUp();
-  overlay.addEventListener('click', function(event) {
-    
+
+  overlay.addEventListener('click', function (event) {
     if (event.target === overlay) {
       closePopUp();
     }
   });
+
   var closeButton = document.querySelector('.close-popup');
   if (closeButton) {
-    closeButton.addEventListener('click', function() {
+    closeButton.addEventListener('click', function () {
       closePopUp();
     });
   }
+
   var buttonStart = document.querySelector('.button-start');
   var playerInfo = document.querySelector('.player-info');
   var gameTable = document.querySelector('.game-table');
-  buttonStart.addEventListener('click', function() {
-      var playerName = document.getElementById('player-name').value;
-      var timeLimit = document.getElementById('time-limit').value;
-      var errorMessageDiv = document.getElementById('error-message');
-      errorMessageDiv.textContent = '';
-      if (playerName.length < 3) {
-          errorMessageDiv.textContent = 'El nombre del jugador debe tener al menos 3 caracteres.';
-          return;
-      }
-      localStorage.setItem('playerName', playerName);
-      localStorage.setItem('timeLimit', timeLimit);
 
-      playerInfo.style.display = 'none';
-      gameTable.style.display = 'flex';
+  buttonStart.addEventListener('click', function () {
+    var playerName = document.getElementById('player-name').value;
+    var timeLimitSelect = document.getElementById('time-limit');
+    var timeLimit = parseInt(timeLimitSelect.value, 10) || 60;
+    var errorMessageDiv = document.getElementById('error-message');
+    errorMessageDiv.textContent = '';
+
+    if (playerName.length < 3) {
+      errorMessageDiv.textContent =
+        'El nombre del jugador debe tener al menos 3 caracteres.';
+      return;
+    }
+
+    localStorage.setItem('playerName', playerName);
+    localStorage.setItem('timeLimit', timeLimit);
+
+    playerInfo.style.display = 'none';
+    gameTable.style.display = 'flex';
+
+    startCountdown(timeLimit);
   });
+
+  function startCountdown(timeLimit) {
+    var startTime = Date.now();
+    var endTime = startTime + timeLimit * 1000;
+
+    countdownInterval = setInterval(function () {
+      var now = Date.now();
+      var timeLeft = endTime - now;
+
+      if (timeLeft <= 0) {
+        clearInterval(countdownInterval);
+        timerDisplay.textContent = 'Tiempo agotado';
+
+        // Emitir evento de tiempo agotado
+        var timeUpEvent = new CustomEvent('timeUp');
+        document.dispatchEvent(timeUpEvent);
+
+        return;
+      }
+
+      var secondsLeft = Math.ceil(timeLeft / 1000);
+      var minutes = Math.floor(secondsLeft / 60);
+      var seconds = secondsLeft % 60;
+
+      timerDisplay.textContent =
+        minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    }, 1000);
+  }
 });
