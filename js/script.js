f4154c84fcc9962f85f9e72c996c26d3194472a1
Nehuen <bovynnehuen@gmail.com>
2024-08-07 00:28:00 -0300

    arreglo de html contacto

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
   redirectToGithub();
-  redirectToOtherPag('btn-start', 'pages/game.html');
-  redirectToOtherPag('contacto-btn', '/pages/contacto.html');
+  redirectToOtherPag('btn-start', './pages/game.html');
+  const contactoBtn = document.getElementById('contacto-btn');
+  if (contactoBtn) {
+      contactoBtn.addEventListener('click', function(event) {
+          const currentPath = window.location.pathname;
+          const contactPath = '/pages/contacto.html';
+  
+          if (currentPath !== contactPath) {
+              event.preventDefault(); 
+              window.location.href = contactPath; 
+          }
+      });
+  }
 });
 
+
 function redirectToGithub() {
-  document.getElementById('github-btn').onclick = function() {
-    window.open('https://github.com/NehuenBovyn/DAW-TP-FINAL', '_blank');
-  };
+  const githubBtn = document.getElementById('github-btn');
+  if (githubBtn) {
+    githubBtn.onclick = function() {
+      window.open('https://github.com/NehuenBovyn/DAW-TP-FINAL', '_blank');
+    };
+  }
 }
 
 function redirectToOtherPag(idHtmlElement, url) {
-  var element = document.getElementById(idHtmlElement);
+  const element = document.getElementById(idHtmlElement);
+  console.log(idHtmlElement, url);
+
   if (element) {
     element.addEventListener('click', function(event) {
       event.preventDefault();
